75f1bf01d2cd0cb65d4148772883afe181318281
jagadeeshgeerla <jagadeesh.geerla@gmail.com>
2019-09-09 23:42:28 +0530

    CodeFirst Example

---
+++ CodeFirstExample/CodeFirstDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace CodeFirstDemo
+{
+   public class Post
+    {
+        public int Id { get; set; }
+        public DateTime DatePublished { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+
+    public class BlogDbContext:DbContext
+    {
+        // public BlogDbContext():base("CodeFirstDemo")
+        public BlogDbContext() : base("name=BlogDbContext")
+        {
+
+        }
+        public DbSet<Post> posts { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
