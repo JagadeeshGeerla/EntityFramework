b09c30ebd67d2ccdd618daa97587d659cb996dd2
jagadeeshgeerla <jagadeesh.geerla@gmail.com>
2019-09-11 21:36:33 +0530

    Seeding the database

---
+++ CodeFirstExistingDB/CodeFirstExistingDB/Migrations/201909111559319_PopulateCategoriesTable.cs
@@ -0,0 +1,17 @@
+namespace CodeFirstExistingDB.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoriesTable : DbMigration
+    {
+        public override void Up()
+        {
+            //Sql("Insert into Categories...");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- CodeFirstExistingDB/CodeFirstExistingDB/Migrations/Configuration.cs
+++ CodeFirstExistingDB/CodeFirstExistingDB/Migrations/Configuration.cs
@@ -1,6 +1,7 @@
 namespace CodeFirstExistingDB.Migrations
 {
     using System;
+    using System.Collections.ObjectModel;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -18,6 +19,16 @@ namespace CodeFirstExistingDB.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+            context.Authors.AddOrUpdate(a => a.Name,
+                new Author
+                {
+                    Name = "Author 1",
+                    Courses = new Collection<Course>()
+                    {
+                        new Course() { Name="Course for Auth1", Description="desc"}
+                    }
+                });
         }
     }
 }
